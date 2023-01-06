8ebc56b57e2e9c8ab3af3cadbc0752237e2705d7
Civiana <120310439+Civiana@users.noreply.github.com>
2023-01-06 23:52:52 +0300

    Update AssemblyInfo.cs

--- Platform Game/Properties/AssemblyInfo.cs
+++ Platform Game/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Platform Game Tutorial MOO ICT")]
+[assembly: AssemblyTitle("Platform Game")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Platform Game Tutorial MOO ICT")]
+[assembly: AssemblyProduct("Platform Game")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
